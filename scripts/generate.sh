c18095a71241780ab5ff8a3cb112ddb3b6455447
iwauo <iwauo.tajima@gmail.com>
2020-06-15 23:02:58 +0900

    update the scripts

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -29,6 +29,7 @@ CONTENT_FILES=$([ -z $UPDATES_SCRIPTS_ONLY ] && echo '.editorconfig .gitattribut
 
 RECURSION_COUNT=1
 
+#@main@
 main() {
   create_next_content_dir
   update_file_index
@@ -47,6 +48,7 @@ main() {
     git diff --no-index $NEXT_CONTENT_DIR $PROJECT_BASE_DIR
   fi
 }
+#@main@
 
 create_next_content_dir() {
   rm -rf $NEXT_CONTENT_DIR $PREV_CONTENT_DIR
@@ -128,7 +130,6 @@ generate() {
     $schema_option \
     --model-files $(normalize_path 'model/') \
     --template-files $(normalize_path 'template/') \
-    --template-files $(normalize_path 'dest/template') \
     --target-dir "$NEXT_CONTENT_DIR_NAME" \
     --local-repo "$LOCAL_MODULE_REPOSITORY"
 }
